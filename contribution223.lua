72f2b6e3cfa899d4f2245fd5b6fd275f446deecf
inf2021223 <inf2021223@ionio.gr>
2022-11-16 15:14:30 +0000

    added book

--- contribution223.lua
+++ contribution223.lua
@@ -1,13 +1,12 @@
 function Image(img)
     if img.classes:find('contribution223',1) then
-      local f = io.open("contribution223/" .. img.src, 'r')
+      local f = io.open("book/" .. img.src, 'r')
       local doc = pandoc.read(f:read('*a'))
       f:close()
-      local contribution = pandoc.utils.stringify(doc.meta.contribution) or "Epigraph has not been set"
-      local student = pandoc.utils.stringify(doc.meta.student) or "Student has not been set"
-      local id = pandoc.utils.stringify(doc.meta.id) or "Student ID has not been set"
-      local credentials = " Student: " .. student .. " (" .. id .. ")"
-      local text = "\n\n _" .. contribution223 .. "_ \n\n"
-      return pandoc.RawInline('markdown',text .. credentials)
+      local contribution = pandoc.utils.stringify(doc.meta.contribution) 
+      local author = pandoc.utils.stringify(doc.meta.author)
+      local id = pandoc.utils.stringify(doc.meta.id)
+      local content = "> _" .. caption .. "_ \n>" .. "Ονοματεπώνυμο Φοιτητή:" .. author .. "Αριθμός Μητρώου:" .. id 
+      return pandoc.RawInline('markdown',content)
     end
 end
